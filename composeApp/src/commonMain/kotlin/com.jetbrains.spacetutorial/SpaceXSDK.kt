c08f8677627bd3a1573c3f735981548de901ae54
GioGutierrez07 <s20120005@alumnos.itsur.edu.mx>
2024-05-18 01:41:59 -0600

    se agrego el spacexapi y el spacexsdk

--- composeApp/src/commonMain/kotlin/com.jetbrains.spacetutorial/SpaceXSDK.kt
+++ composeApp/src/commonMain/kotlin/com.jetbrains.spacetutorial/SpaceXSDK.kt
@@ -1,4 +1,26 @@
 package com.jetbrains.spacetutorial
 
-class SpaceXSDK {
+import com.jetbrains.spacetutorial.cache.DataBase
+import com.jetbrains.spacetutorial.cache.DatabaseDriverFactory
+import com.jetbrains.spacetutorial.network.SpaceXApi
+import com.jetbrains.spacetutotial.cache.DataBase
+import com.jetbrains.spacetutotial.cache.DatabaseDriverFactory
+
+
+class SpaceXSDK (databaseDriverFactory: DatabaseDriverFactory, val api: SpaceXApi) {
+    private val database = DataBase(databaseDriverFactory)
+
+
+
+    @Throws(Exception::class)
+    suspend fun getLaunches(forceReload: Boolean): List<RocketLaunch> {
+        val cachedLaunches = database.getAllLaunches()
+        return if (cachedLaunches.isNotEmpty() && !forceReload) {
+            cachedLaunches
+        } else {
+            api.getAllLaunches().also {
+                database.clearAndCreateLaunches(it)
+            }
+                   }
+            }
 }
